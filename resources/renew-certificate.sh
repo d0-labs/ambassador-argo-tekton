2455a221df2aa81a2266c15ef4573738fbf7246f
avillela <50256412+avillela@users.noreply.github.com>
2021-01-14 17:54:04 -0500

    Update renew-certificate.sh
    Added comments to explain what's going on in key sections

--- resources/renew-certificate.sh
+++ resources/renew-certificate.sh
@@ -5,30 +5,38 @@
 
 set -e
 
+# Delete the Certificate and its accompanying Secret
 export CERT_NAME=$(kubectl get certificates -n ambassador -o name | cut -d'/' -f 2)
 export ORIGINAL_CERT_EXPIRY=$(kubectl get certificate -n ambassador -o=jsonpath='{.items[0].status.notAfter}')
 echo "Certificate expires on $ORIGINAL_CERT_EXPIRY"
+
+# Use date -d if you're using non-BSD/MacOS
 export ORIGINAL_CERT_EXPIRY_DATE=$(date -j -f "%F" $ORIGINAL_CERT_EXPIRY +"%s")
+
 kubectl delete certificate $CERT_NAME -n ambassador
 
 export AMBASSADOR_CERTS_SECRET=$(kubectl get secrets -n ambassador -o=jsonpath='{.items[0].metadata.name}')
 kubectl delete secret $AMBASSADOR_CERTS_SECRET -n ambassador
 
+# Just for kicks
 export CERT_MANAGER_POD_NAME=$(kubectl get pods -n cert-manager -o=jsonpath='{.items[0].metadata.name}')
 echo Pod name: $CERT_MANAGER_POD_NAME
 
-# See: https://gist.github.com/avillela/d220ad085502eb475ab6415b8b4ad208
+# The only resource that should change when you apply this file is the Certificate, as it will be re-created
 kubectl apply -f cluster_setup/ambassador-tls-cert-issuer.yml
 
+# May take a bit of time for the certificate to generate. Putting in a pause.
 echo "Sleeping for 40 seconds to give the certificate time to generate"
 sleep 40
 
+# Get our new certificat's expiry date
+# Use date -d if you're using non-BSD/MacOS
 kubectl describe certificates ambassador-certs -n ambassador
 export NEW_CERT_EXPIRY=$(kubectl get certificate -n ambassador -o=jsonpath='{.items[0].status.notAfter}')
 echo "New Certificate expires on $NEW_CERT_EXPIRY"
 export NEW_CERT_EXPIRY_DATE=$(date -j -f "%F" $NEW_CERT_EXPIRY +"%s")
 
-# https://unix.stackexchange.com/questions/84381/how-to-compare-two-dates-in-a-shell
+# We want to make sure that the new certificate's expiry date is after our old certificate's expiry date
 if [ $NEW_CERT_EXPIRY_DATE -ge $ORIGINAL_CERT_EXPIRY_DATE ];
 then
     echo "Success! New certificate generated. New expiry date is $NEW_CERT_EXPIRY"
@@ -37,6 +45,7 @@ else
     exit 1
 fi
 
+# Let's make sure that the secret accompanying our certificate was also created
 export SECERT_NAME=$(kubectl get secrets -n ambassador ambassador-certs -o name)
 if [ "$SECERT_NAME" == "secret/ambassador-certs" ];
 then
